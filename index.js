e0eb0bbd344357b35504ed3ab40607458386d3d6
jasnmar <jasnmar@gmail.com>
2024-04-06 09:11:58 -0400

    Event Listener

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import { tweetsData } from "./data.js";
 
 console.log(tweetsData);
+
+const tweetInput = document.getElementById("tweet-input")
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener("click", function(event) {
+    console.log(tweetInput.value)
+})
