31f2b9f65afd91628df5103fe4484f167764c7fd
jasnmar <jasnmar@gmail.com>
2024-04-06 10:19:59 -0400

    Like event listener

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ tweetBtn.addEventListener("click", function(event) {
     console.log(tweetInput.value)
 })
 
+document.addEventListener("click", function(event) {
+    console.log(event.target.dataset.like)
+})
+
 function getFeedHtml() {
     let feedHTML = ""
     tweetsData.forEach(tweet => {
@@ -21,15 +25,15 @@ function getFeedHtml() {
                     <p class="tweet-text">${tweet.tweetText}</p>
                     <div class="tweet-details">
                         <span class="tweet-detail">
-                            <i class="fa-regular fa-comment-dots"></i>
+                            <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
                             ${tweet.replies.length}
                         </span>
                         <span class="tweet-detail">
-                            <i class="fa-solid fa-heart"></i>
+                            <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
                             ${tweet.likes}
                         </span>
                         <span class="tweet-detail">
-                            <i class="fa-solid fa-retweet"></i>
+                            <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
                             ${tweet.retweets}
                         </span>
                     </div>   
